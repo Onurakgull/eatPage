5e8d39fa0d1027919e7611e7f1aff7046fe7b8e4
onurakgl <65688719+onurakgl@users.noreply.github.com>
2020-11-14 17:52:22 +0300

    yemek sayfası

---
+++ app.js
@@ -0,0 +1,8 @@
+var swiper = new Swiper('.component--1 .swiper-container', {
+    slidesPerView: 3,
+    spaceBetween: 10,
+    navigation: {
+        nextEl: '.component--1 .leftbtn',
+        prevEl: '.component--1 .rightbtn',
+      },
+  });
